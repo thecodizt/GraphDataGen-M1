d4ef3845ebe6ed3215769f3d6a14b8c2fce5c46e
Dev <dev@thecodizt.com>
2024-02-14 15:18:26 +0530

    ADD: Base files

---
+++ app.py
@@ -0,0 +1,149 @@
+import streamlit as st
+import yaml
+from scipy.interpolate import interp1d
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go 
+
+
+def create_series(control_points, y_range, cycles, noise=0.05 ):
+
+    # Sort control points by x value
+    # Sort control points by y value
+    control_points.sort()
+
+    # Generate x values for each control point
+    ys = control_points
+    xs = np.linspace(0, 1, len(ys))
+
+    # Create spline
+    spline = interp1d(xs, ys, kind='quadratic')
+
+    # Generate x values for each day in the date range
+    x = np.linspace(0, 1, num=cycles, endpoint=True)
+
+    # Generate y values (spline)
+    y = spline(x)
+
+    y += np.random.normal(0, noise, size=y.shape)
+
+    # Apply Min-Max scaling to y values
+    y_min, y_max = np.min(y), np.max(y)
+    y = (y - y_min) / (y_max - y_min)  # Min-Max scaling
+
+    # Scale y values to provided y range
+    y = y * (y_range[1] - y_range[0]) + y_range[0]
+
+    # Create a pandas Series with dates as the index
+    spline_series = pd.Series(y)
+
+    return spline_series
+
+def generate_independent_subnodes(config, super_node_id, cycles):
+    n_subnodes = config['n_subnodes']
+    boundaries = config['boundaries']
+    control_points = config['control_points']
+
+    subnodes = {}
+
+    for i in range(n_subnodes):
+        subnodes[i] = create_series(control_points, boundaries[i], cycles)
+
+        st.download_button(
+            label=f'Download for Sub Node {i}',
+            data=pd.DataFrame(subnodes).to_csv(),
+            file_name=f'subnode_{super_node_id}_{i}.csv',
+            mime='text/csv'
+        )
+
+        show_preview = st.toggle("Show Preview", value=False, key=f"subnode_{super_node_id}_{i}")
+
+        if show_preview:
+            st.table(subnodes[i])
+
+
+    fig = px.line(pd.DataFrame(subnodes))
+    st.plotly_chart(fig)
+
+    return subnodes
+
+def generate_dependent_subnodes(config, super_node_id, cycles, generated_series):
+    n_subnodes = config['n_subnodes']
+    boundaries = config['boundaries']
+    control_points = config['control_points']
+
+    # TODO: Fix logic
+    subnodes = {}
+
+    for i in range(n_subnodes):
+        
+        base = create_series(control_points, boundaries[i], cycles)
+
+        for j in range(config['n_incomming_nodes']):
+
+            value = config['inputs'][j]
+
+            # st.write(generated_series[value['input_supernode']][0])
+
+            input_series = pd.Series(generated_series[value['input_supernode']][0])
+
+            # st.table(generated_series[value['input_supernode']][0])
+
+            base += input_series * value['weight'] * value['correlation']
+
+            # subnodes[i] = input_series
+
+
+        subnodes[i] = base
+
+        st.download_button(
+            label=f'Download for Sub Node {i}',
+            data=pd.DataFrame(subnodes).to_csv(),
+            file_name=f'subnode_{super_node_id}_{i}.csv',
+            mime='text/csv'
+        )
+
+        show_preview = st.toggle("Show Preview", value=False, key=f"subnode_{super_node_id}_{i}")
+
+        if show_preview:
+            st.table(subnodes[i])
+
+    # st.table(subnodes)
+    fig = px.line(pd.DataFrame(subnodes))
+    st.plotly_chart(fig) 
+
+    return subnodes
+
+def main():
+    st.title("Data Generator - Graph Based")
+
+    config = None
+
+    uploaded_file = st.file_uploader("Upload YAML Configuration File", type=['yaml', 'yml'])
+
+    if uploaded_file is not None:
+        file_contents = uploaded_file.read()
+        config = yaml.safe_load(file_contents)
+
+    if config is not None:
+        generated_series = {}
+
+        for key, value in config['supernodes'].items():
+
+            if value['node_type'] == 'independent':
+                st.header(f"Independent Super Node {key+1}")
+
+                generated_series[key] = generate_independent_subnodes(value, key, config['n_cycles'])
+
+        for key, value in config['supernodes'].items():
+            if value['node_type'] == 'dependent':
+                st.header(f"Dependent Super Node {key+1}")
+
+                generated_series[key] = generate_dependent_subnodes(value, key, config['n_cycles'], generated_series)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ config_generator.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import yaml
+
+def get_matrix_input(n, m):
+    input_matrix = st.text_input(f"Enter the 2D matrix of shape {n}x{m} values separated by comma, columns seperated by |", key=f"input_matrix_{n}_{m}")
+    if input_matrix != "":
+        input_matrix = input_matrix.split('|')
+        input_matrix = [i.split(',') for i in input_matrix]
+        input_matrix = [[float(j) for j in i] for i in input_matrix]
+
+        if len(input_matrix) != n or len(input_matrix[0]) != m:
+            st.error(f"Shape of input matrix should be {n}x{m}")
+        else:
+            return input_matrix
+
+def main():
+    st.title("Data Generator - Graph Based - Config Generator")
+
+    super_nodes_data = {}
+
+    super_nodes_data['n_supernodes'] = st.number_input("Enter number of super nodes", 1, step=1)
+
+    super_nodes_data['n_cycles'] = st.number_input("Enter number of cycles", 100, step=100)
+
+    super_nodes_data['supernodes'] = {}
+ 
+
+    for i in range(super_nodes_data['n_supernodes']):
+
+        st.subheader(f"Super Node {i+1}")
+
+        n_nodes = st.number_input(f"Enter number of sub nodes for Super Node {i+1}", 1, step=1)
+
+        control_points = []
+
+        control_points_in = st.text_input(f"Enter comma separated control points for Super Node {i+1}")
+
+        if control_points_in:
+            control_points = [float(x) for x in control_points_in.split(',')]
+
+        node_type = st.selectbox(f"Select type for Super Node {i+1}", ['independent', 'dependent'])
+
+        
+        if node_type == 'dependent':
+            n_incoming_nodes = st.number_input(f"Enter number of incoming nodes for Super Node {i+1}", 1, step=1)
+
+            inputs = []
+
+            for j in range(n_incoming_nodes):
+                
+                st.subheader(f"Incomming Node {j+1} for Super Node {i+1}")
+
+                input_supernode = st.selectbox(f"Incomming Node {j+1} for Super Node {i+1}", list(super_nodes_data['supernodes'].keys()))
+
+                if input_supernode is not None:
+                    correlation = st.number_input(f"Correlation for Incomming Node {j+1} for Super Node {i+1}", -1.0, 1.0, 0.0)
+                    weight = st.number_input(f"Weight for Incomming Node {j+1} for Super Node {i+1}", 0.0, 1.0, 0.0)
+                    
+                    connection = st.text_input(f"Enter comma separated connections for Incomming node {j+1} for Super Node {i+1}")
+
+                    connections = list(int(x) for x in connection.split(',')) if connection else []
+
+                    expected_lower_bound = st.number_input(f"Enter expected lower bound for Incomming node {j+1} for Super Node {i+1}")
+                    expected_upper_bound = st.number_input(f"Enter expected upper bound for Incomming node {j+1} for Super Node {i+1}")
+                    expected_mean = st.number_input(f"Enter expected mean for Incomming node {j+1} for Super Node {i+1}")
+
+                    inputs.append({
+                        'input_supernode': input_supernode,
+                        'correlation': correlation,
+                        'weight': weight,
+                        'connections': connections,
+                        'expected_lower_bound': expected_lower_bound,
+                        'expected_upper_bound': expected_upper_bound,
+                        'expected_mean': expected_mean
+                    })
+        else:
+            n_incoming_nodes = 0
+            inputs = []
+        
+        boundaries = []
+        for j in range(n_nodes):
+            bounds = st.text_input(f"Enter comma separated bounds for Sub Node {j+1} for Super Node {i+1}")
+
+            if bounds:
+                bounds = [float(x) for x in bounds.split(',')]
+            else:
+                bounds = [0,1]
+
+            boundaries.append(bounds)
+
+        super_nodes_data['supernodes'][i] = {
+            'node_type': node_type,
+            'n_subnodes': n_nodes,
+            'boundaries': boundaries,
+            'control_points': control_points,
+            'n_incomming_nodes': n_incoming_nodes,
+            'inputs': inputs
+        }
+
+
+
+
+    # Convert the dictionary into a YAML formatted string
+    yaml_config = yaml.dump(super_nodes_data)
+
+    # Create a download button for the YAML configuration file
+    st.download_button(
+        label="Download YAML Configuration File",
+        data=yaml_config,
+        file_name='config.yaml',
+        mime='application/x-yaml'
+    )
+
+if __name__ == "__main__":
+    main()
